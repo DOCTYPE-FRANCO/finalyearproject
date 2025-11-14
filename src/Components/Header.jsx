143076445f9d12761fba3c2f6dbf27f88c3891f0
Franco <ufrancis186@gmail.com>
2025-11-14 20:45:07 +0100

    x

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return(
-        <div className="top-4 fixed ">
+        <div className="top-4 fixed w-[90vw] h-[50px] bg-blue-600/10">
 
         </div>
     );
