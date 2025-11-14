d07e22cd988441870e86e60066f70b0dcfb5cee3
Franco <ufrancis186@gmail.com>
2025-11-14 22:37:43 +0100

    landing page almost done

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -9,18 +9,18 @@ function Homepage() {
             <Herosection />
 
             <div className="flex justify-center gap-16 md:flex-row  mt-24">
-                <div className="flex flex-col gap-5 justify-center items-center shadow-2xl shadow-gray-700 w-[200px] h-[200px] rounded-md">
-                    <img src={Secure} className="w-[100px]"/>
+                <div className="flex flex-col gap-5 justify-center items-center shadow-2xl shadow-gray-700 w-[200px] h-[170px] rounded-md">
+                    <img src={Secure} className="w-[70px]"/>
                     <p className="font-bold">Secure Digital Voting</p>
                 </div>
 
-                <div className="flex flex-col gap-5 justify-center items-center shadow-2xl shadow-gray-700 w-[200px] h-[200px] rounded-md">
-                    <img src={Robot} className="w-[100px]"/>
+                <div className="flex flex-col gap-5 justify-center items-center shadow-2xl shadow-gray-700 w-[200px] h-[170px] rounded-md">
+                    <img src={Robot} className="w-[70px]"/>
                     <p className="font-bold">Automated Vote Count</p>
                 </div>
 
-                <div className="flex flex-col gap-5 justify-center items-center shadow-2xl shadow-gray-700 w-[200px] h-[200px] rounded-md">
-                    <img src={Access} className="w-[100px]"/>
+                <div className="flex flex-col gap-5 justify-center items-center shadow-2xl shadow-gray-700 w-[200px] h-[170px] rounded-md">
+                    <img src={Access} className="w-[70px]"/>
                     <p className="font-bold">Multi-Level Access</p>
                 </div>
             </div>
