a1ee9efc3ad4cf3b2fca18c5e2d3a48768995e69
Franco <ufrancis186@gmail.com>
2025-11-14 20:41:49 +0100

    still setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Homepage from './Components/Homepage'
+import Header from './Components/Header'
 
 function App() {
     <div>
+      <Header />
       <Routes>
-        <Route />
+        <Route path='/' element={<Homepage/>} />
       </Routes>
     </div>
 }
+
 
 export default App
