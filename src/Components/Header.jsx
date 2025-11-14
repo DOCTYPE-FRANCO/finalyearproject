84b3c09bccf22433bc62abde3f0fbf76e8905434
Franco <ufrancis186@gmail.com>
2025-11-14 21:28:23 +0100

    started building out the landing page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Logo from "../assets/SE-logo.png"
 
 function Header() {
     return(
-        <div className="flex items-center justify-between top-4 fixed w-[90vw] h-[50px] bg-gray-600/10 z-50 left-1/2 transform -translate-x-1/2 rounded-2xl">
-            header
+        <div className="flex items-center justify-between top-4 fixed w-[80vw] h-[50px] bg-gray-600/10 z-50 left-1/2 transform -translate-x-1/2 rounded-2xl">
+            <div className="ml-4">
+                <img src={Logo} className="w-[65px]" />
+            </div>
+
+            <p className="text-sm flex justify-center items-center rounded-sm mr-7 bg-blue-500 w-[120px] h-[30px] text-white font-bold hover:scale-110 transition-all duration-300 hover:cursor-pointer">GET STARTED</p>
         </div>
     );
 }
--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 function Herosection (){
     return(
-        <div></div>
+        <div>
+            <div>
+                <p>Secure, Fast & Transperent Digital Voting for Schools and Organizations</p>
+                <p>A modern platform that enables students and staff to vote digitally, with maximum security , providing ease in result collation </p>
+            </div>
+        </div>
     );
 }
 
