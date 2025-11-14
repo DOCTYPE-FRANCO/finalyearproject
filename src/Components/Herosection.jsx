37b79b4754552ce12949e6d365fb1fa602d8284a
Franco <ufrancis186@gmail.com>
2025-11-14 21:49:30 +0100

    x

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -2,9 +2,14 @@ import React from "react";
 function Herosection (){
     return(
         <div>
-            <div>
-                <p>Secure, Fast & Transperent Digital Voting for Schools and Organizations</p>
-                <p>A modern platform that enables students and staff to vote digitally, with maximum security , providing ease in result collation </p>
+            <div className="flex flex-col gap-8 justify-center items-center">
+                <p className="text-5xl font-bold md:max-w-[900px] text-center">Secure, Fast & Transperent Digital Voting for Organizations</p>
+                <p className="md:max-w-[570px] text-center text-blue-800 font-thin">A secure and user-friendly digital voting platform for students and staff. It streamlines elections with encrypted voting, simple access from any device, and fast, accurate result collation. The system ensures integrity, transparency, and a smooth voting experience for the entire institution.</p>
+            </div>
+
+            <div className="flex justify-center pt-6">
+                <button className="w-[130px] h-[45px] bg-blue-600 rounded-sm text-white">GET STARTED</button>
+                <button className="w-[130px] h-[45px] border border-blue-600 rounded-sm text-blue-600"></button>
             </div>
         </div>
     );
