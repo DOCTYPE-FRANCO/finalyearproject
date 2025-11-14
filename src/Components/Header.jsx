183d7494d154918947f496882115220e2456e95c
Franco <ufrancis186@gmail.com>
2025-11-14 21:03:31 +0100

    x

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return(
-        <div className="top-4 fixed w-[90vw] h-[50px] bg-white/10 z-50">
+        <div className="flex items-center justify-between top-4 fixed w-[90vw] h-[50px] bg-gray-600/10 z-50 left-1/2 transform -translate-x-1/2 rounded-2xl">
             header
         </div>
     );
