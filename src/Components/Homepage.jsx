8ba9608e5a30c31feffe622c6afa023e9169291a
Franco <ufrancis186@gmail.com>
2025-11-16 18:12:05 +0100

    x

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -29,20 +29,28 @@ function Homepage() {
 
             <div>
                 <div className="flex md:flex-row justify-center gap-5 mt-10">
-                    <div className="w-[250px] h-[250px] bg-white flex items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
+                    <div className="w-[250px] h-[250px] bg-white flex flex-col items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
                         <div className="flex justify-center items-center text-2xl text-white bg-blue-600 w-[60px] h-[60px] rounded-full">1</div>
+                        <p className="text-2xl font-bold">Create Election</p>
+                        <p className="text-center text-sm font-thin">Admin sets positions, candidates, and timelines</p>
                     </div>
 
-                     <div className="w-[250px] h-[250px] bg-white flex items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
+                     <div className="w-[250px] h-[250px] bg-white flex flex-col items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
                         <div className="flex justify-center items-center text-2xl text-white bg-blue-600 w-[60px] h-[60px] rounded-full">2</div>
+                        <p className="text-2xl font-bold">Create Election</p>
+                        <p className="text-center text-sm font-thin">Admin sets positions, candidates, and timelines</p>
                     </div>
 
-                     <div className="w-[250px] h-[250px] bg-white flex items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
+                     <div className="w-[250px] h-[250px] bg-white flex flex-col items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
                         <div className="flex justify-center items-center text-2xl text-white bg-blue-600 w-[60px] h-[60px] rounded-full">3</div>
+                        <p className="text-2xl font-bold">Create Election</p>
+                        <p className="text-center text-sm font-thin">Admin sets positions, candidates, and timelines</p>
                     </div>
 
-                     <div className="w-[250px] h-[250px] bg-white flex items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
+                     <div className="w-[250px] h-[250px] bg-white flex flex-col items-center justify-center shadow-2xl shadow-gray-400 rounded-md">
                         <div className="flex justify-center items-center text-2xl text-white bg-blue-600 w-[60px] h-[60px] rounded-full">4</div>
+                        <p className="text-2xl font-bold">Create Election</p>
+                        <p className="text-center text-sm font-thin">Admin sets positions, candidates, and timelines</p>
                     </div>
                 </div>
             </div>
