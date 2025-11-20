bf7f0d693d0467d38fa005f3aea08c72da71e56f
Franco <ufrancis186@gmail.com>
2025-11-20 21:29:14 +0100

    x

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
                 <img src={Logo} className="w-[65px]" />
             </div>
 
-            <Link to="election">
+            <Link to="/election">
                 <p className="text-sm flex justify-center items-center rounded-sm mr-7 bg-blue-500 w-[120px] h-[30px] text-white font-bold hover:scale-110 transition-all duration-300 hover:cursor-pointer">GET STARTED</p>
             </Link>
         </div>
