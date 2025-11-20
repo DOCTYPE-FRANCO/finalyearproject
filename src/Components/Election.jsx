4398fc642b8867fa3aab7d07275ca25a6e8260e7
Franco <ufrancis186@gmail.com>
2025-11-20 21:54:13 +0100

    x

--- src/Components/Election.jsx
+++ src/Components/Election.jsx
@@ -1,12 +1,12 @@
-import { Plus } from "lucide-react";
+import { CrossIcon, Plus, PlusCircle } from "lucide-react";
 import React from "react";
 
 function Election() {
     return(
         <div className="mt-20">
-            <div className="flex flex-col justify-center items-center w-[300px] h-[250px] shadow-gray-400 shadow-2xl rounded-md">
+            <div className="flex flex-col gap-3 justify-center items-center w-[270px] h-[200px] shadow-gray-400 shadow-2xl rounded-md">
                 <p className="font-extrabold text-blue-600 text-2xl">CREATE ELECTION</p>
-                <Plus  color="#0000FF" size={64}/>
+                <PlusCircle  color="#2563EB" size={54}/>
             </div>
         </div>
     );
