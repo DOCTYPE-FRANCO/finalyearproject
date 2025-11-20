076f09063f67a675502fbdc8c35711ba268a769b
Franco <ufrancis186@gmail.com>
2025-11-20 21:45:13 +0100

    x

--- src/Components/Election.jsx
+++ src/Components/Election.jsx
@@ -1,9 +1,13 @@
+import { Plus } from "lucide-react";
 import React from "react";
 
 function Election() {
     return(
         <div>
-            
+            <div className="flex flex-col w-[350px] h-[300px] shadow-gray-400 shadow-2xl">
+                <Plus />
+                <p>Create Election</p>
+            </div>
         </div>
     );
 }
