0baebf9475bee16abfad4d304c82c367836416f9
Franco <ufrancis186@gmail.com>
2025-11-20 21:48:36 +0100

    x

--- src/Components/Election.jsx
+++ src/Components/Election.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 function Election() {
     return(
-        <div>
-            <div className="flex flex-col w-[350px] h-[300px] shadow-gray-400 shadow-2xl">
-                <Plus />
-                <p>Create Election</p>
+        <div className="mt-20">
+            <div className="flex flex-col justify-center items-center w-[300px] h-[250px] shadow-gray-400 shadow-2xl rounded-md">
+                <p className="font-extrabold text-blue-600 text-2xl">CREATE ELECTION</p>
+                <Plus  color="#0000FF" size={64}/>
             </div>
         </div>
     );
