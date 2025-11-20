029420839f790e33b9e92e150a285c4649b12dca
Franco <ufrancis186@gmail.com>
2025-11-20 22:28:24 +0100

    x

--- src/Components/Election.jsx
+++ src/Components/Election.jsx
@@ -3,27 +3,27 @@ import React from "react";
 
 function Election() {
     return(
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <p className="text-center font-extrabold text-blue-600 text-3xl mt-20">CREATE NEW ELECTION OR SELECT AN EXISTING TEMPLATE</p>
-            <div className="mt-20 grid md:grid-cols-3 pl-20">
-                <div className="flex flex-col gap-5 justify-center items-center w-[270px] h-[200px] shadow-gray-400 shadow-2xl rounded-md">
-                    <p className="font-extrabold text-blue-600 text-2xl">CREATE ELECTION</p>
+            <div className="grid md:grid-cols-2 gap-5 pt-16">
+                <div className="flex flex-col gap-5 justify-center items-center w-[330px] h-[150px] shadow-gray-400 shadow-2xl rounded-md">
                     <PlusCircle  color="#2563EB" size={54}/>
+                    <p className="font-extrabold text-blue-600 text-2xl">CREATE ELECTION</p>   
                 </div>
 
-                <div className="flex flex-col gap-5 justify-center items-center w-[270px] h-[200px] shadow-gray-400 shadow-2xl rounded-md">
-                    <p className="font-extrabold text-blue-600 text-2xl">FACUSSA ELECTIONS</p>
+                <div className="flex flex-col gap-5 justify-center items-center w-[330px] h-[150px] shadow-gray-400 shadow-2xl rounded-md">
                     <School  color="#2563EB" size={54}/>
+                    <p className="font-extrabold text-blue-600 text-2xl">FACUSSA ELECTIONS</p>     
                 </div>
 
-                <div className="flex flex-col gap-5 justify-center items-center w-[270px] h-[200px] shadow-gray-400 shadow-2xl rounded-md">
-                    <p className="font-extrabold text-blue-600 text-center text-2xl">ACCOUNTING ELECTIONS</p>
+                <div className="flex flex-col gap-5 justify-center items-center w-[330px] h-[150px] shadow-gray-400 shadow-2xl rounded-md">
                     <School2  color="#2563EB" size={54}/>
+                    <p className="font-extrabold text-blue-600 text-center text-2xl">ACCOUNTING ELECTIONS</p>   
                 </div>
 
-                <div className="flex flex-col gap-5 justify-center items-center w-[270px] h-[200px] shadow-gray-400 shadow-2xl rounded-md">
-                    <p className="font-extrabold text-blue-600 text-center text-2xl">ALTAR SERVERS ELECTIONS</p>
+                <div className="flex flex-col gap-5 justify-center items-center w-[330px] h-[150px] shadow-gray-400 shadow-2xl rounded-md">
                     <Church  color="#2563EB" size={54}/>
+                    <p className="font-extrabold text-blue-600 text-center text-2xl">ALTAR SERVERS ELECTIONS</p>   
                 </div>
             </div>
         </div>
